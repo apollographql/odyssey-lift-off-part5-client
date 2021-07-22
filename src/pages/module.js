ae5ba31caf3bae332c811c19713d59c20f5d0583
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2021-07-22 10:56:27 -0600

    initial commit from lift-off IV code

---
+++ src/pages/module.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, ModuleDetail, QueryResult } from '../components';
+
+/**
+ * GET_MODULE_AND_PARENT_TRACK gql query to retrieve a specific module and its parent track,
+ * both needed for the ModuleDetail component
+ */
+export const GET_MODULE_AND_PARENT_TRACK = gql`
+  query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
+    module(id: $moduleId) {
+      id
+      title
+      content
+      videoUrl
+    }
+    track(id: $trackId) {
+      id
+      title
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`;
+
+/**
+ * Module page fetches both parent track and module's data from the gql query GET_MODULE_AND_PARENT_TRACK
+ * and feeds them to the ModuleDetail component
+ */
+const Module = ({ moduleId, trackId }) => {
+  const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
+    variables: { moduleId, trackId },
+  });
+
+  return (
+    <Layout fullWidth>
+      <QueryResult error={error} loading={loading} data={data}>
+        <ModuleDetail track={data?.track} module={data?.module} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Module;
---
+++ src/pages/track.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import TrackDetail from '../components/track-detail';
+
+/** GET_TRACK gql query to retrieve a specific track by its ID */
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
+
+/**
+ * Track Page fetches a track's data from the gql query GET_TRACK
+ * and provides it to the TrackDetail component to display
+ */
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
