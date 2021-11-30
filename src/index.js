4539f13902e906790e644f5e1a49c0be84f94620
Megan Sullivan <meganesulli@gmail.com>
2021-11-30 13:15:09 -0800

    chore: hard-code to always use prod server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
-const serverUri = process.env.NODE_ENV === "production" ? 'https://catstronauts-server-lift-off-5.herokuapp.com/' : 'http://localhost:4000'
+const serverUri = 'https://catstronauts-server-lift-off-5.herokuapp.com/'
 console.log(process.env.NODE_ENV)
 
 const client = new ApolloClient({
