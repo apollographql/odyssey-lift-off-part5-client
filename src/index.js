e308e02057b1a4a03a8e0c85fab4cfdcdf5d76d3
Megan Sullivan <meganesulli@gmail.com>
2021-11-30 12:47:28 -0800

    feat: update server URL to use prod

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+const serverUri = process.env.NODE_ENV === "production" ? 'https://catstronauts-server-lift-off-5.herokuapp.com/' : 'http://localhost:4000'
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: serverUri,
   cache: new InMemoryCache(),
 });
 
