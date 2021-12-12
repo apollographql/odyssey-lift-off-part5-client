7837d1f7f0811b9ebe6abe4648861f5dae2732d5
yunnie <steve.s.yun@gmail.com>
2021-12-12 12:39:03 -0800

    changed uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://cats-apollo-launch.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
