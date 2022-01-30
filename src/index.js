a8f55e0d03efbbbc4fc63e043ff00631bb33c443
anjali-shah <60100633+anjali-shah@users.noreply.github.com>
2022-01-30 23:10:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://graphqlpoc.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
