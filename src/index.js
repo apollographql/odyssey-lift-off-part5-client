b13fde5084e894af0872b96b0d13f14adbc6fa78
Wendy Beck <wbeck@simplehealth.com>
2022-04-20 16:54:43 -0700

    deploying client to heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://apollo-v2.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
