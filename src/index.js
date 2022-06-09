28efc3c5c9e9078cd1fc997988610e02ebbf00d6
Lucas Lo <lucaslo@creasant.com>
2022-06-09 17:08:31 -0400

    Change uri

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "https://lucas-server-catstronauts.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
@@ -14,5 +14,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
