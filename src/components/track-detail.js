5ad8359bdc13eab1a19d9370aa632c1de4f7f2e7
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-07-05 16:22:51 -0400

    revert: use length instead of durationInSeconds --- durationInSeconds usage should only be in the final branch, not the starter code

--- src/components/track-detail.js
+++ src/components/track-detail.js
@@ -19,16 +19,7 @@ import MarkDown from './md-content';
  * It provides access to the first module of the track.
  */
 const TrackDetail = ({ track }) => {
-  const {
-    title,
-    description,
-    thumbnail,
-    author,
-    durationInSeconds,
-    modulesCount,
-    modules,
-    numberOfViews,
-  } = track;
+  const { title, description, thumbnail, author, length, modulesCount, modules, numberOfViews } = track;
 
   return (
     <ContentSection>
@@ -50,7 +41,7 @@ const TrackDetail = ({ track }) => {
             </IconAndLabel>
             <IconAndLabel>
               <IconTime width="14px" />
-              <div>{humanReadableTimeFromSeconds(durationInSeconds)}</div>
+              <div>{humanReadableTimeFromSeconds(length)}</div>
             </IconAndLabel>
           </DetailItem>
           <DetailItem>
@@ -77,9 +68,7 @@ const TrackDetail = ({ track }) => {
               {modules.map((module) => (
                 <li key={module.title}>
                   <div>{module.title}</div>
-                  <ModuleLength>
-                    {humanReadableTimeFromSeconds(module.durationInSeconds)}
-                  </ModuleLength>
+                  <ModuleLength>{humanReadableTimeFromSeconds(module.length)}</ModuleLength>
                 </li>
               ))}
             </ul>
--- src/pages/track.js
+++ src/pages/track.js
@@ -15,13 +15,13 @@ const GET_TRACK = gql`
         photo
       }
       thumbnail
-      durationInSeconds
+      length
       modulesCount
       numberOfViews
       modules {
         id
         title
-        durationInSeconds
+        length
       }
       description
     }
