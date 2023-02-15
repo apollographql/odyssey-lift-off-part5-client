7cbe00433e851a16a5b0e5022f9df1f7759186d7
Flaviu Cismas <flaviucisdev@gmail.com>
2023-02-15 17:49:39 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-part5-server-production-03f1.up.railway.app", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-03f1.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
+  name: "web",
+  version: "1.0",
 });
 
 ReactDOM.render(
